33414975895d8ba29a9c509080a477b514d96f23
Gonzalo <wiki@softcial.com>
2023-10-12 12:45:14 -0600

    change on footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -123,7 +123,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Softcial. Built with Docusaurus 2. (Wiki v1.0)`,
       },
       prism: {
         theme: lightCodeTheme,
