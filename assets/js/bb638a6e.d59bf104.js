ee4484d96727b27446f8f4850e1985f8c03d1305
Gonzalo Guillen <wiki@softcial.com>
2023-10-12 10:33:36 -0600

    Updates

--- assets/js/bb638a6e.d59bf104.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkknowledge_base=self.webpackChunkknowledge_base||[]).push([[2433],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ assets/js/d2591a21.6c0b0d8a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkknowledge_base=self.webpackChunkknowledge_base||[]).push([[1359],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
