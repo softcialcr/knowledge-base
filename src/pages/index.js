aac7ed952977468a23552dbeaea41cced189a418
Gonzalo <wiki@softcial.com>
2023-10-12 12:04:23 -0600

    fix on colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/middlit/middlit-getting-started">
+            Getting started
           </Link>
         </div>
       </div>
